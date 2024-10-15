1e5ecad9b2134e8b9d2b12c2b4ef968c5a5c8a45
Inguss <inguss.brikmanis@gmail.com>
2024-10-15 22:21:16 +0200

    Update analyze.sql

--- analyze.sql
+++ analyze.sql
@@ -30,7 +30,6 @@ SELECT COUNT(day_of_week) as ride_count, day_of_week, member_casual from filtere
 GROUP BY (day_of_week, member_casual)
 ORDER by ride_count DESC
 
---Member usage aligns with a weekday commuting pattern, while casual users are more active on weekends, likely for leisure purposes.
 
 --Top choice per bike type:
 
@@ -38,7 +37,6 @@ SELECT COUNT (rideable_type) AS count, rideable_type, member_casual FROM filtere
 GROUP BY (rideable_type, member_casual)
 ORDER BY count DESC
 
---The high usage of electric bikes among both member and casual users suggests a preference for the convenience and speed they offer, especially for longer trips. Classic bikes remain a staple.
 
 --AVG trip length:
 
@@ -47,15 +45,12 @@ SELECT AVG (trip_length_m) avg_length, member_casual FROM filtered_data
 GROUP BY (member_casual)
 ORDER BY (avg_length) DESC
 
---Casuals take double the longer trips, meaning weekend leisure trips vs members - to work/other quick rides
 
 --AVG trip length per month:
 
 SELECT AVG (trip_length_m) avg_length, month, member_casual FROM filtered_data
 GROUP BY (member_casual, filtered_data.month)
 ORDER BY (month) DESC
-
---Casual users consistently have longer average trip durations compared to members, with the highest trip length seen during the summer months (June, July, and August). This aligns with expected patterns where warm-weather months see more recreational use, particularly from casual riders.
 
 
 
@@ -70,4 +65,3 @@ ORDER BY (rides) DESC
 DELETE FROM  filtered_data
 WHERE MONTH = 'MAY'
 
---The data indicates that summer is the most active season for rides, with July being the peak month. Casual users are more seasonal in their usage, heavily favoring the summer months, while members maintain more steady activity across the year. The data suggests that ride-sharing services might consider focusing promotional efforts for casual users during summer and maintaining engagement with members year-round.
